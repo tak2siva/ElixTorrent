cefe143eb827623a6453ece5e427ddfb0048e72f
Shiva <tak2siva@gmail.com>
2015-10-11 20:11:37 +0530

    parse and show torrent meta data

--- lib/elix_torrent.ex
+++ lib/elix_torrent.ex
@@ -1,2 +1,14 @@
 defmodule ElixTorrent do
+    @torrent_file "ubuntu-14.04.3-desktop-amd64.iso.torrent"
+
+    def start(_type, _args) do
+        IO.puts "Starting ElixTorrent"
+        Task.start(read_torrent)
+    end
+
+    def read_torrent do
+        meta_data = File.read @torrent_file
+        parsed_data = Bencode.decode elem(meta_data,1)
+        IO.inspect elem(parsed_data, 1)
+    end
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule ElixTorrent.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [mod: {ElixTorrent, []}, applications: [:logger, :elixir_bencode]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule ElixTorrent.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:elixir_bencode, "~> 1.0.0"}]
   end
 end
